e136c81ec53f56048051722dbdb85e59632dbd01
Crackpot <jt.wagner@outlook.de>
2018-01-28 22:55:50 +0100

    Collisions Platformer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,25 @@ public class Player : MonoBehaviour {
     public float timeToJumpApex = .3f;
     public float moveSpeed = 6;
 
+
     float accelerationTimeAirborne = .01f;
     float accelerationTimeGrounded = .005f;
 
     float gravity;
-    float jumpVelocitySmall;
+    float jumpVelocity;
     float velocityXSmoothing;
 
     Vector3 velocity;
 
     Controller2D controller2D;
-
-    bool jumpKeyPressed = false;
-    float jumKeyPressedTime = 0;
+    
 
 	// Use this for initialization
 	void Start () {
         controller2D = GetComponent<Controller2D>();
 
         gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex,2);
-        jumpVelocitySmall = Mathf.Abs(gravity) * timeToJumpApex;
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 
 
     }
@@ -42,32 +41,11 @@ public class Player : MonoBehaviour {
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         
-        
-
-        if (Input.GetKeyDown(KeyCode.Space) && controller2D.collisions.below && jumpKeyPressed == false) {
-            // initial small jump
-            velocity.y = jumpVelocitySmall;
-            jumpKeyPressed = true;
-            jumKeyPressedTime = Time.time;
-        } else {
-            if (jumpKeyPressed == true && Input.GetKey(KeyCode.Space)) {
-                    
-                float keyPressedTime = (Time.time - jumKeyPressedTime);
-                if (keyPressedTime < 1f) {
-                    float smallInterval = 0.4f;
-                    velocity.y += smallInterval;
-                    Debug.Log("Added small jump interval: " + velocity.y);
-                }
-            }
+        if (Input.GetKeyDown(KeyCode.Space) && controller2D.collisions.below) {
+            
+            velocity.y = jumpVelocity;            
         }
-        
-
-
-        // Wenn Jump key losgelassen wird, wieder in den ursprungszustand
-        if (Input.GetKeyUp(KeyCode.Space) && jumpKeyPressed) {
-            Debug.Log("Jump Key losgelassen");
-            jumpKeyPressed = false;
-        }
+                
 
         float targetVelocityX = input.x * moveSpeed;
 
